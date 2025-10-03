57ba3efba242a0d116a3d079a05cff9fadbd4696
Lavanya Bharani <lbharani99@gmail.com>
2025-10-03 00:37:38 -0400

    fix contact links alignment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,63 +1,64 @@
-import '../App.css';
-import React from 'react'; 
+import "../App.css";
+import React from "react";
+
+function contactLink(href, imgSrc, text) {
+  return (
+    <a
+      href={href}
+      style={{
+        fontSize: "0.8em",
+        display: "flex",
+        flexWrap: "nowrap",
+        marginRight: "15px",
+      }}
+    >
+      <img
+        src={imgSrc}
+        style={{
+          width: "20px",
+          height: "20px",
+          display: "flex",
+          marginRight: "5px",
+        }}
+        alt={text}
+      />
+      <p style={{ flexWrap: "nowrap" }}>{text}</p>
+    </a>
+  );
+}
 
 function Home() {
   return (
     <div className="MainContent">
       <h1>Lavanya Bharani</h1>
-      <div 
-        style={{ 
-          display: "inline-flex", 
-          justifyContent: "center",  // or "space-between", "space-around"
-          gap: "10px",              // optional spacing between links
-          marginBottom: "20px",      // optional spacing below
-          flexWrap: "nowrap"
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center", // or "space-between", "space-around"
+          marginBottom: "20px", // optional spacing below
         }}
->
-      {/* Email */}
-      <a
-        href="mailto:lbharani99@gmail.com"
-        style={{ fontSize: "0.8em", display: "inline-flex" }}
       >
-        <img 
-          src="email.avif"  
-          style={{ width: "1em", height: "1em", display: "flex", marginRight: "7px" }} 
-          alt="email"
-        />
-        lbharani99@gmail.com 
-      </a>
+        {contactLink(
+          "mailto:lbharani99@gmail.com",
+          "email.avif",
+          "lbharani99@gmail.com"
+        )}
 
-      {/* LinkedIn */}
-      <a
-        href="https://www.linkedin.com/in/lavanya-bharani"
-        style={{ fontSize: "0.8em", display: "inline-flex", marginLeft: "10px" }}
-      >
-        <img 
-          src="linkedin.png"  
-          style={{ width: "0.9em", height: "0.9em", display: "inline-flex", marginRight: "7px" }} 
-          alt="linkedin"
-        />
-        linkedin.com/in/lavanya-bharani 
-      </a>
+        {contactLink(
+          "https://www.linkedin.com/in/lavanya-bharani",
+          "linkedin.png",
+          "LinkedIn"
+        )}
 
-      {/* GitHub */}
-      <a
-        href="https://github.com/laviosa99"
-        style={{ fontSize: "0.8em", display: "inline-flex", marginLeft: "10px" }}
-      >
-        <img 
-          src="github.png"  
-          style={{ width: "0.9em", height: "0.9em", display: "inline", marginRight: "7px" }} 
-          alt="github"
-        />
-        github.com/laviosa99
-      </a>
+        {contactLink("https://github.com/laviosa9", "github.png", "Github")}
       </div>
       <p>
-        My name is Lavanya and I'm an engineer, baker, and plant-mom. I've just graduated with my Master's in Data Science and am looking for my next adventure in tech! This is a space for me to share projects, accomplishments, and hobbies with the world.
+        My name is Lavanya and I'm an engineer, baker, and plant-mom. I've just
+        graduated with my Master's in Data Science and am looking for my next
+        adventure in tech! This is a space for me to share projects,
+        accomplishments, and hobbies with the world.
       </p>
-
-      
     </div>
   );
 }
